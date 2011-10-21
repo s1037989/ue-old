4ed20f78f2e6b2ff0a8280af1118acda61593236
Stefan Adams <stefan@cogentinnovators.com>
2011-10-21 09:49:35 -0500

    no more need for backup_ubuntu_environment.sh

--- root/backup_ubuntu_environment.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-tar cvzf ubuntu_environment_$(date +"%Y%m%d%H%M%S").tar.gz /etc/profile.d /etc/skel /usr/local /root/{*.sh,packages.log,.bashrc,.profile} /home/local/{.bashrc,.profile} /etc/joe/{joerc,ftyperc} /etc/lynx-cur/lynx.cfg
