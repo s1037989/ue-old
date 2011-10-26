f5c01bd138c39322874a0698d8544a396fac1c34
Stefan Adams <stefan@borgia.com>
2011-10-26 18:51:18 -0500

    Update root/reseal.sh

--- root/reseal.sh
+++ root/reseal.sh
@@ -9,8 +9,8 @@ perl -pi -e '$_="" if /KERNEL.*?eth/' /etc/udev/rules.d/70-persistent-net.rules
 rm -f /etc/ubuntu-firstrun
 rm -f /home/local/{.bash_history,.pwd,.sudo_as_admin_successful} /home/local/.ssh/*
 rm -rf /home/local/.cache
-rm -f /root/{.bash_history,.joe_state,.pwd,.lesshst,.viminfo,.gitconfig} /root/.ssh/{authorized_keys,id_rsa,id_rsa.pub,config} /.git
-rm -rf /root/{.aptitude,.debtags}
+rm -f /root/{.bash_history,.joe_state,.pwd,.lesshst,.viminfo,.gitconfig} /root/.ssh/{authorized_keys,id_rsa,id_rsa.pub,config}
+rm -rf /.git /root/{.aptitude,.debtags}
 rm -f /tmp/dhclient.env /var/lib/dhcp3/dhclient.* /tmp/checkip
 /usr/local/lib/cemosshe/cemosshe.uninstall ; rm -rf /usr/local/lib/cemosshe/uninstall
 /usr/local/lib/snarsshe/snarsshe.uninstall
